5d22bfb84285ec8ad367981b2a41a1f6a1c8b1cb
Sagar Das <Sagar.uiet8@gmail.com>
2025-10-02 15:56:03 -0400

    removing archived code

--- archive/unused_components/smart_chunker_modular/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Smart Chunker Module
-
-Main orchestrator for multi-language code chunking with semantic awareness.
-"""
-
-from .smart_chunker import SmartChunker
-
-__all__ = ['SmartChunker']
